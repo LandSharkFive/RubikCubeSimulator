e1c304890835a67431426cc09e1847cd51e58464
Bruce Koivu <153857026+LandSharkFive@users.noreply.github.com>
2024-03-09 10:57:57 -0500

    Folder
    Format

---
+++ TestOne/TestMove.cs
@@ -0,0 +1,62 @@
+﻿
+using CubeOne;
+
+namespace TestOne
+{
+    [TestClass]
+    public class Test_Move
+    {
+        // Test Rotate Function.
+
+        [TestMethod]
+        public void Test_Rotate_Front_Left()
+        {
+            Cube c = new Cube();
+            string? s1 = c.GetState();
+            c.Move("FLlf");
+            string? s2 = c.GetState();
+            Assert.AreEqual(s1, s2);
+        }
+
+        [TestMethod]
+        public void Test_Rotate_Front_Right()
+        {
+            Cube c = new Cube();
+            string? s1 = c.GetState();
+            c.Move("FRrf");
+            string? s2 = c.GetState();
+            Assert.AreEqual(s1, s2);
+        }
+
+        [TestMethod]
+        public void Test_Rotate_Front_Up()
+        {
+            Cube c = new Cube();
+            string? s1 = c.GetState();
+            c.Move("FUuf");
+            string? s2 = c.GetState();
+            Assert.AreEqual(s1, s2);
+        }
+
+        [TestMethod]
+        public void Test_Rotate_Front_Down()
+        {
+            Cube c = new Cube();
+            string? s1 = c.GetState();
+            c.Move("FDdf"); ;
+            string? s2 = c.GetState();
+            Assert.AreEqual(s1, s2);
+        }
+
+        [TestMethod]
+        public void Test_Rotate_Front_Back()
+        {
+            Cube c = new Cube();
+            string? s1 = c.GetState();
+            c.Move("FBbf"); ;
+            string? s2 = c.GetState();
+            Assert.AreEqual(s1, s2);
+        }
+
+    }
+}
---
+++ TestOne/TestOppositeSides.cs
@@ -0,0 +1,51 @@
+﻿using CubeOne;
+
+namespace TestOne
+{
+
+    [TestClass]
+    public class TestOppositeSides
+    {
+        // Check opposite sides.  The Intersection is empty.
+
+        [TestMethod]
+        public void Test_Up_Down()
+        {
+            Cube c = new Cube();
+            string? s1 = c.GetState();
+            c.UpMove();
+            c.DownMove();
+            c.AntiUpMove();
+            c.AntiDownMove();
+            string? s2 = c.GetState();
+            Assert.AreEqual(s1, s2);
+        }
+
+        [TestMethod]
+        public void Test_Left_Right()
+        {
+            Cube c = new Cube();
+            string? s1 = c.GetState();
+            c.LeftMove();
+            c.RightMove();
+            c.AntiLeftMove();
+            c.AntiRightMove();
+            string? s2 = c.GetState();
+            Assert.AreEqual(s1, s2);
+        }
+
+        [TestMethod]
+        public void Test_Front_Back()
+        {
+            Cube c = new Cube();
+            string? s1 = c.GetState();
+            c.FrontMove();
+            c.BackMove();
+            c.AntiFrontMove();
+            c.AntiBackMove();
+            string? s2 = c.GetState();
+            Assert.AreEqual(s1, s2);
+        }
+
+    }
+}
